7935580c73a6d43fce19fc08382d0764e00ecedd
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-09-29 13:11:26 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,36 @@
-variable "rg_name" { type = string }
+variable "environment" { type = string }
+variable "rg_prefix" { type = string }
 variable "location" { type = string }
 variable "name_prefix" { type = string }
 
 variable "vnet_cidr" { type = string }
 variable "subnet_cidr" { type = string }
 
+# VM
 variable "vm_size" { type = string }
 variable "admin_username" { type = string }
 variable "ssh_public_key_path" { type = string }
 variable "os_disk_size_gb" { type = number }
 
-variable "storage_account_name" { type = string }
+# Storage
+variable "storage_account_prefix" { type = string }
 variable "storage_container_name" { type = string }
 
-variable "dns_zone_name" { type = string }
-variable "dns_record_name" { type = string }
+# AWS Route53
+variable "aws_region" { type = string }
+variable "hosted_zone_name" { type = string }
+variable "record_prefix" { type = string }
 variable "dns_record_ttl" { type = number }
 
-variable "tags" {
-  type = map(string)
-}
+# Tags
+variable "tags" { type = map(string) }
 
-# PostgreSQL vars
+# PostgreSQL
 variable "pg_admin_username" { type = string }
 variable "pg_admin_password" { type = string }
-variable "pg_database_name"  { type = string }
+variable "pg_database_name" { type = string }
+variable "pg_version" {
+  description = "PostgreSQL Flexible Server version"
+  type        = string
+  default     = "15"
+}
