a6b5e29fc39a64814f37f1dc8cbd9ad5384d8998
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-09-29 13:10:28 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,14 +1,21 @@
-/*terraform {
-  required_version = ">= 1.5.0"
-
+terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.113"
+      version = ">= 3.53.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
     }
   }
+  required_version = ">= 1.5.0"
 }
 
 provider "azurerm" {
   features {}
-}*/
+}
+
+provider "aws" {
+  region = var.aws_region
+}
