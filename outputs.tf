52bfd07a252ec7189824b8e1b9db584222c585ab
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-09-29 13:09:44 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,40 @@
 output "public_ip" {
-  value = azurerm_public_ip.vm_pip.ip_address
+  description = "Public IP of the Azure VM"
+  value       = azurerm_public_ip.vm_pip.ip_address
 }
 
 output "private_ip" {
-  value = azurerm_network_interface.nic.ip_configuration[0].private_ip_address
+  description = "Private IP of the Azure VM"
+  value       = azurerm_network_interface.nic.ip_configuration[0].private_ip_address
 }
 
 output "postgresql_hostname" {
-  value = azurerm_postgresql_flexible_server.pg.fqdn
+  description = "FQDN of the PostgreSQL Flexible Server"
+  value       = azurerm_postgresql_flexible_server.pg.fqdn
 }
 
-output "storage_account_name" {
-  value = azurerm_storage_account.sa.name
+output "resource_group" {
+  description = "Resource Group Name"
+  value       = azurerm_resource_group.rg.name
 }
 
-output "storage_account_region" {
-  value = azurerm_storage_account.sa.location
+output "azure_dns_record" {
+  description = "FQDN of the Azure VM in AWS Route53"
+  value       = aws_route53_record.azure_vm_record.fqdn
+}
+
+# ---------- TFE related outputs ----------
+output "tfe_object_storage_type" {
+  description = "Object storage type for TFE"
+  value       = "azure"
+}
+
+output "tfe_object_storage_account_name" {
+  description = "Azure Storage Account used for TFE"
+  value       = azurerm_storage_account.sa.name
+}
+
+output "tfe_object_storage_container" {
+  description = "Azure Storage Container used for TFE"
+  value       = azurerm_storage_container.tfe_container.name
 }
